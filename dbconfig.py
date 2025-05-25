4db44ec05b067a024a42206dbdf4bf3b466c98c6
Angela Davis <angela__davis@outlook.com>
2025-05-25 18:05:24 +0100

    Fixing the update button showing incorrectly in the create form

--- dbconfig.py
+++ dbconfig.py
@@ -1,16 +1,16 @@
 # For using pythonanywhere
-mysql = {
-    'host':"adproject.mysql.pythonanywhere-services.com",
-    'user':"adproject",
-    'password':"wsaaprojectsql",
-    'database':"adproject$wsaadb"
-}
+#mysql = {
+#    'host':"adproject.mysql.pythonanywhere-services.com",
+#    'user':"adproject",
+#    'password':"wsaaprojectsql",
+#    'database':"adproject$wsaadb"
+#}
 
 
 # For testing locally
-#mysql = {
-#    'host':"localhost",
-#    'user':"root",
-#    'password':"",
-#    'database':"dproject$wsaadb"
-#}
+mysql = {
+    'host':"localhost",
+    'user':"root",
+    'password':"",
+    'database':"dproject$wsaadb"
+}
